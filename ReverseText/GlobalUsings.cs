7a78f51dcec5d0aaa8c219cde3cd5cfbe1a0caa6
Hyeon Kim <hkim399@my.bcit.ca>
2022-09-08 14:45:28 -0700

    first commit

---
+++ ReverseText/GlobalUsings.cs
@@ -0,0 +1,2 @@
+// global using ReverseText;
+global using System;
---
+++ ReverseText/Program.cs
@@ -0,0 +1,29 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+namespace ca.bcit;
+
+// using ReverseText;
+// using System;
+using MyLibrary;
+
+/// this is documentation
+
+public class Booboo {
+	public static void Main(String[] args) {
+		Console.Write("Please enter your name: ");
+		// Scanner scanner = new Scanner(System.in);
+		String? name = Console.ReadLine();
+
+        Console.WriteLine(Helper.Reverse(name!));
+
+	}
+
+    // private static string reverse(string s)
+    // {
+    //     char[] charArray = s.ToCharArray();
+    //     Array.Reverse(charArray);
+    //     return new string(charArray);
+    // }
+}
